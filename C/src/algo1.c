3368fd0dfc0127d1f96e73678cc9246a3f23d2be
ndiab <3502158@ppti-14-302-01.ufr-info-p6.jussieu.fr>
2017-01-14 19:33:45 +0100

    ajout de openMP de la premiere partie

--- C/src/algo1.c
+++ C/src/algo1.c
@@ -6,11 +6,11 @@ unsigned long long int algo1(Context *cont)
 {  
     cont->start = my_gettimeofday(); 
 
-    int i,j;
-    unsigned long long int y_min, surface_max = 0;
+    int i,j,k;
+    unsigned long long int y_min = (unsigned long long int)cont->h, surface_max = 0;
 
-    #pragma omp parallel
-    for (i=0;i<cont->nb_points-1;++i)     
+    for (i=0;i<cont->nb_points-1;++i)
+	#pragma omp parallel for private(j,k) lastprivate(y_min)      
         for (j=i+1;j<cont->nb_points;++j)
         {
            if (j==i+1)
@@ -20,12 +20,11 @@ unsigned long long int algo1(Context *cont)
            else // au moins deux points separe xi et xj (i.e: xi< p1<p2 <xj)
            { 
               y_min = cont->Points[i+1][1];
-	      int k;
               for (k=i+1;k<=j-1;++k)
                   y_min = MIN(y_min, cont->Points[k][1]);
-           }
-           surface_max = MAX(surface_max,y_min*(cont->Points[j][0] - cont->Points[i][0]));
         }
+        surface_max = MAX(surface_max,y_min*(cont->Points[j][0] - cont->Points[i][0]));
+    }
     cont->end = my_gettimeofday();
   
     return surface_max;
--- C/src/algo2.c
+++ C/src/algo2.c
@@ -1,9 +1,13 @@
 #include <algo2.h>
-
+#include <omp.h>
 
 unsigned long long int algo2(Context *cont){
+
+    cont->start = my_gettimeofday(); 
+
     int i,j;
-    unsigned long long y_min = cont->h , surface_max = 0; 
+    unsigned long long y_min = cont->h , surface_max = 0;
+    #pragma omp parallel for private(j) firstprivate(y_min) reduction(max:surface_max)
     for (i=0;i<cont->nb_points-1;++i)
         for (j=i+1;j<cont->nb_points;++j)
         {
@@ -13,5 +17,8 @@ unsigned long long int algo2(Context *cont){
               y_min = MIN(y_min, cont->Points[j-1][1]);
            surface_max = MAX(surface_max,y_min*(cont->Points[j][0] - cont->Points[i][0]));
          }  
+
+    cont->end = my_gettimeofday();
+
     return surface_max;
 }
